4ef69d8818d641a16b28c7c6da702f38c82ded9c
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-11-22 18:53:43 +0100

    implemented level and view handling

--- CropViewToRoom/Command.cs
+++ CropViewToRoom/Command.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 #endregion
 
@@ -31,9 +33,37 @@ namespace CropViewToRoom
       {
         tx.Start( "Create cropped views for each room" );
 
+        string date_iso = DateTime.Now.ToString( "yyyy-MM-dd" );
+
         foreach( Level level in levels )
         {
           Debug.Print( level.Name );
+
+          ElementId id_view = level.FindAssociatedPlanViewId();
+          ViewPlan view = doc.GetElement( id_view ) as ViewPlan;
+
+          IEnumerable<Room> rooms
+            = new FilteredElementCollector( doc, id_view )
+              .OfClass( typeof( SpatialElement ) )
+              .Where<Element>( e => e is Room )
+              .Cast<Room>();
+
+          foreach( Room room in rooms )
+          {
+            string view_name = string.Format( 
+              "{0}_cropped_to_room_{1}_date_{2}",
+              view.Name, room.Name, date_iso );
+
+            id_view = view.Duplicate( 
+              ViewDuplicateOption.AsDependent );
+
+            View view_cropped = doc.GetElement( id_view ) 
+              as View;
+
+            view_cropped.Name = view_name;
+
+
+          }
         }
         tx.Commit();
       }
