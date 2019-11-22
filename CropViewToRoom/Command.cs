0298c8df5fd74feef44d65b353b384c0fb0c28fa
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-11-22 18:38:09 +0100

    added description and implemented skeleton add-in

---
+++ CropViewToRoom/Command.cs
@@ -0,0 +1,43 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace CropViewToRoom
+{
+  [Transaction( TransactionMode.Manual )]
+  public class Command : IExternalCommand
+  {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Application app = uiapp.Application;
+      Document doc = uidoc.Document;
+
+      FilteredElementCollector levels
+        = new FilteredElementCollector( doc )
+          .OfClass( typeof( Level ) );
+
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Create cropped views for each room" );
+
+        foreach( Level level in levels )
+        {
+          Debug.Print( level.Name );
+        }
+        tx.Commit();
+      }
+      return Result.Succeeded;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CropViewToRoom
-For each level, collect all rooms and generate views cropped to each room
+
+Revit C# .NET add-in that, for each level, collects all rooms and generates views cropped to each room, to address
+the [Revit API discussion forum](http://forums.autodesk.com/t5/revit-api-forum/bd-p/160) threads
+on [generating views from Rooms](https://forums.autodesk.com/t5/revit-api-forum/generate-views-from-rooms/m-p/9144449)
+and [CreateViaOffset](https://forums.autodesk.com/t5/revit-api-forum/createviaoffset/m-p/9159500).
+
+
+## Author
+
+Jeremy Tammik, [The Building Coder](http://thebuildingcoder.typepad.com), [ADN](http://www.autodesk.com/adn) [Open](http://www.autodesk.com/adnopen), [Autodesk Inc.](http://www.autodesk.com)
+
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+Please see the [LICENSE](LICENSE) file for full details.
+
